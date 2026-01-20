8e24c2f9bcc54b9864794373f6fed5226067685b
zhangminghui <2501603832@qq.com>
2026-01-20 17:45:14 +0800

    更新版本

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,7 +6,7 @@ export const SITE_CONFIG = {
     title: '耶温博客',
     description: '记录思考，分享知识',
     keywords: '耶温博客, 技术博客, 编程, 开发, 前端, 后端, 全栈',
-    author: '耶温',
+    author: '提莫',
     url: 'https://www.yuwb.dev',
     copyright: {
         startYear: 2024,
